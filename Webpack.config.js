4ab263798db6bbe80fc102e13994a833a0231d77
jblsoftware <56892829+jblsoftware@users.noreply.github.com>
2020-05-11 17:26:09 +0100

    Update Webpack.config.js

--- Webpack.config.js
+++ Webpack.config.js
@@ -1,15 +1,23 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackBuild = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = ({ mode, presets } = { mode: "development", presets: [] }) => {
   return {
     mode,
-    entry: './src/index.ts',
-    devtool: 'inline-source-map',
+    entry: {
+      app: "./src/index.ts",
+    },
+    devtool: "inline-source-map",
+    devServer: {
+      contentBase: "./dist",
+      hot: true,
+      host: "localhost",
+    },
     output: {
-      filename: "bundle.js",
       path: path.resolve(__dirname, "dist"),
+      filename: "[name].bundle.js",
     },
     module: {
       rules: [
@@ -20,6 +28,12 @@ module.exports = ({ mode, presets } = { mode: "development", presets: [] }) => {
         },
       ],
     },
-    plugins: [new HtmlWebpackBuild(), new webpack.ProgressPlugin()],
+    plugins: [
+      new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+      new HtmlWebpackBuild({
+        title: "Development",
+      }),
+      new webpack.ProgressPlugin(),
+    ],
   };
 };
